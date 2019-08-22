cdbe42928376e3414f6d8c7c239227f7932c61e4
fanjindong <765912710@qq.com>
2019-08-22 15:03:48 +0800

    fix: demo

--- example/amqp.go
+++ example/amqp.go
@@ -1,4 +1,4 @@
-package db
+package example
 
 import (
 	"fmt"
@@ -9,27 +9,18 @@ import (
 	"github.com/streadway/amqp"
 )
 
-//DramatiqConn is ...
-var DramatiqConn *amqp.Connection
+//Conn is ...
+var Conn *amqp.Connection
 
-// NodeConn is ...
-var NodeConn *amqp.Connection
+// ChannelPool is ...
+var ChannelPool pool.Pool
 
-// DramatiqChannelPool is ...
-var DramatiqChannelPool pool.Pool
-
-// NodeChannelPool is ...
-var NodeChannelPool pool.Pool
 var err error
 
 // Init is ...
 func Init() {
-	config := conf.GetConfig()
-	DramatiqConn, err = amqp.Dial(config["amqp_url"].(string))
-	if err != nil {
-		panic("Failed to connect to RabbitMQ")
-	}
-	NodeConn, err := amqp.Dial(config["amqp_node_url"].(string))
+
+	Conn, err = amqp.Dial("amqp://root:password@127.0.0.1:5672/")
 	if err != nil {
 		panic("Failed to connect to RabbitMQ")
 	}
@@ -49,31 +40,24 @@ func Init() {
 		//连接最大空闲时间，超过该时间的连接 将会关闭，可避免空闲时连接EOF，自动失效的问题
 		IdleTimeout: 10 * time.Second,
 	}
-	DramatiqChannelPool, err = pool.NewChannelPool(poolConfig)
+	ChannelPool, err = pool.NewChannelPool(poolConfig)
 	if err != nil {
 		fmt.Println("err=", err)
 	}
+	//从连接池中取得一个连接
+	v, err := ChannelPool.Get()
 
-	//factory 创建连接的方法
-	factoryNode := func() (interface{}, error) { return NodeConn.Channel() }
-	//创建一个连接池： 初始化2，最大连接30
-	poolConfigNode := &pool.Config{
-		InitialCap: 2,
-		MaxCap:     20,
-		Factory:    factoryNode,
-		Close:      close,
-		//Ping:       ping,
-		//连接最大空闲时间，超过该时间的连接 将会关闭，可避免空闲时连接EOF，自动失效的问题
-		IdleTimeout: 10 * time.Second,
-	}
-	NodeChannelPool, err = pool.NewChannelPool(poolConfigNode)
-	if err != nil {
-		fmt.Println("err=", err)
-	}
+	//将连接放回连接池中
+	ChannelPool.Put(v)
+
+	//释放连接池中的所有连接
+	ChannelPool.Release()
+
+	//查看当前连接中的数量
+	current := ChannelPool.Len()
 }
 
 // Close is ...
 func Close() {
-	DramatiqConn.Close()
-	NodeConn.Close()
+	Conn.Close()
 }
